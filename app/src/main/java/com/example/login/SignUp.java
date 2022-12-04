2057634b5846a971aa95b421bb893ba18d7bafb5
sid142003 <97834759+sid142003@users.noreply.github.com>
2022-12-04 11:10:30 +0530

    update 1.1

--- app/src/main/java/com/example/login/SignUp.java
+++ app/src/main/java/com/example/login/SignUp.java
@@ -10,5 +10,6 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        getSupportActionBar().hide();
     }
 }
